38780b88a8d1b50b7be16e052520402af6b388f7
LAMENOR <danilosanchesferme@gmail.com>
2021-10-14 16:33:36 -0300

    Adicionado comentarios

--- Assets/Scripts/ManageCartas.cs
+++ Assets/Scripts/ManageCartas.cs
@@ -7,7 +7,10 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class ManageCartas : MonoBehaviour
-{
+{ 
+    /// <summary>
+    /// Mantem todas as condicoes do jogo e governa as regras gerais, usado na cena de game exclusivamente
+    /// </summary>
     public GameObject cartaPrefab; //Carta para se instanciar
     public GameObject[,] cartasArray; //Array/matrix para todas as cartas, separada por linha
     const int maxRankCartas = 13; //Constante do numero de colunas e numero de ranks de cartas
@@ -17,14 +20,14 @@ public class ManageCartas : MonoBehaviour
     public string backCor; //Cor da parte de tras da carta
     public int CorCarta; //Cor da carta relacionada ao naipe
 
-    /*  Qual tipo de jogo ser� jogado, 
+    /*  Qual tipo de jogo sera jogado, 
      * gamemode 0 = 2 Linhas e 2 naipes
      * gamemode 1 = 2 linhas e 1 naipe, dois decks diferentes
      * gamemode 2 = 4 linhas e 4 naipes, 4 cartas iguais
      */
     public int gameMode; 
 
-    public int cartasSelecionadas = 0b0000; //Binario que mantem conta das cartas selecionadas, primeira carta � o primeiro algarismo, segunda o segundo algarismo, etc
+    public int cartasSelecionadas = 0b0000; //Binario bool que mantem conta das cartas selecionadas, primeira carta e o primeiro algarismo, segunda o segundo algarismo, etc
     public GameObject[] cartasSelecionadasArray; //Array para referenciar cartas selecionadas
 
     bool timerPausado, timerAcionado; //Delay para resolucao de acerto ou tentativa
@@ -34,10 +37,15 @@ public class ManageCartas : MonoBehaviour
 
     int recorde = 0; //Recorde de outros jogos
 
+    AudioSource somOK; // Som para tocar ao acertar
+    AudioSource somErro; //Som para tocar ao errar
+
     // Start is called before the first frame update
     void Start()
     {
-
+        /*
+         * Ao dar start as configuracoes sao lidas e resources sao carregados
+         */
         gameMode = PlayerPrefs.GetInt("gamemode", 1);
         if(gameMode == 0)
         { 
@@ -53,12 +61,20 @@ public class ManageCartas : MonoBehaviour
         numeroLinhas = gameMode == 2 ? 4 : 2;
         cartasSelecionadasArray = new GameObject[numeroLinhas];
 
+        somOK = gameObject.AddComponent<AudioSource>();
+        somOK.clip = Resources.Load<AudioClip>("beep");
+        somErro = gameObject.AddComponent<AudioSource>();
+        somErro.clip = Resources.Load<AudioClip>("somErro");
+
         GameObject.Find("ultimaJogada").GetComponent<Text>().text = "Recorde = " + recorde;
         InicializarCartas();
     }
 
     void InicializarCartas()
     {
+        /*
+         * Da setup na mesa do jogo colocando as cartas nos seus devidos lugares
+         */
         GameObject centro = GameObject.Find("centroDaTela");
         cartasArray = new GameObject[numeroLinhas, maxRankCartas];
         
@@ -87,7 +103,9 @@ public class ManageCartas : MonoBehaviour
 
     private GameObject CriarCarta(int rank, int indiceVertical, Vector3 novaPosicao)
     {
-        
+        /*
+         * Cria a carta de acordo com o gamemode e com as configuracoes escolhidas inicialmente
+         */
         var carta = Instantiate(cartaPrefab, novaPosicao, Quaternion.identity, transform);
 
         carta.GetComponent<Tile>().cartaID = rank;
@@ -99,6 +117,7 @@ public class ManageCartas : MonoBehaviour
         string numeroCarta = "";
         string nomeNaipe = "spades";
         string nomeBack = "playCardBack";
+
         if (rank == 0)
             numeroCarta = "ace";
         else if (rank == 10)
@@ -166,6 +185,9 @@ public class ManageCartas : MonoBehaviour
 
     public int[] criaArrayEmbaralhado()
     {
+        /*
+         * Retorna um array embaralhado de numeros entre 0 e 12
+         */
         int[] novaArray = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
         int temp;
         for (int i = 0; i < maxRankCartas; i++)
@@ -180,20 +202,25 @@ public class ManageCartas : MonoBehaviour
 
     public void CartaSelecionada(GameObject carta)
     {
-        //print(cartasSelecionadas & 0b1);
-        if ((cartasSelecionadas & 1) == 0) //Equivalente = primeira carta n�o selecionada
+        /*
+         * Chamada ao clicar na carta, verifica condicoes necessarias para se tornar selecionada, 
+         * caso nao seja da mesma linha adiciona a carta ao array das cartas selecionadas e faz update do bool
+         */
+        if ((cartasSelecionadas & 1) == 0) //Equivalente a primeira carta nao selecionada
         {
-            int linha = int.Parse(carta.name.Substring(0, 1));
+            int linha = int.Parse(carta.name.Substring(0, 1));//Achar linha da carta
             if(LinhasIguais(linha)) return;
-            cartasSelecionadas |= 0b1;
+
+            cartasSelecionadas |= 0b1; //primeira carta
             cartasSelecionadasArray[0] = carta;
             cartasSelecionadasArray[0].GetComponent<Tile>().RevelaCarta();
         }
-        else if((cartasSelecionadas & 0b11) == 0b01) //Equivalente = segunda carta n�o selecionada e os anteriores sim
+        else if((cartasSelecionadas & 0b11) == 0b01) //Equivalente a segunda carta nao selecionada e os anteriores sim
         {
-            int linha = int.Parse(carta.name.Substring(0, 1));
+            int linha = int.Parse(carta.name.Substring(0, 1));//Achar linha da carta
             if (LinhasIguais(linha)) return;
-            cartasSelecionadas |= 0b11;
+
+            cartasSelecionadas |= 0b11; //primera e segunda carta
             cartasSelecionadasArray[1] = carta;
             cartasSelecionadasArray[1].GetComponent<Tile>().RevelaCarta();
             if (gameMode != 2)
@@ -201,20 +228,20 @@ public class ManageCartas : MonoBehaviour
                 VerificaCartas();
                 return;
             }
-        }else if(gameMode == 2 && (cartasSelecionadas & 0b111) == 0b011) //Equivalente = terceira carta n�o selecionada e os anteriores sim
+        }else if(gameMode == 2 && (cartasSelecionadas & 0b111) == 0b011) //Equivalente a terceira carta nao selecionada e os anteriores sim
         {
-            int linha = int.Parse(carta.name.Substring(0, 1));
+            int linha = int.Parse(carta.name.Substring(0, 1));//Achar linha da carta
             if (LinhasIguais(linha)) return;
-            print(cartasSelecionadas);
-            cartasSelecionadas |= 0b111;
-            print(cartasSelecionadas);
+
+            cartasSelecionadas |= 0b111; //primeira segunda e terceira carta
             cartasSelecionadasArray[2] = carta;
             cartasSelecionadasArray[2].GetComponent<Tile>().RevelaCarta();
-        }else if(gameMode == 2 && (cartasSelecionadas & 0b1111) == 0b0111) //Equivalente = quarta carta n�o selecionada e os anteriores sim
+        }else if(gameMode == 2 && (cartasSelecionadas & 0b1111) == 0b0111) //Equivalente a quarta carta nao selecionada e os anteriores sim
         {
             int linha = int.Parse(carta.name.Substring(0, 1));
             if (LinhasIguais(linha)) return;
-            cartasSelecionadas |= 0b1111;
+
+            cartasSelecionadas |= 0b1111; //todas as cartas
             cartasSelecionadasArray[3] = carta;
             cartasSelecionadasArray[3].GetComponent<Tile>().RevelaCarta();
             VerificaCartas();
@@ -222,8 +249,10 @@ public class ManageCartas : MonoBehaviour
 
         bool LinhasIguais(int linha)
         {
-            print(linha);
-            print(cartasSelecionadasArray.Length);
+            /*
+             * Verifica se a carta selecionada mais recente faz parte da linha de outra carta selecionada
+             */
+
             for (int i = 0; i < cartasSelecionadasArray.Length; i++)
             {
                 if (cartasSelecionadasArray[i] && cartasSelecionadasArray[i].GetComponent<Tile>().linhaID == linha)
@@ -238,18 +267,26 @@ public class ManageCartas : MonoBehaviour
 
     void VerificaCartas()
     {
+        /*
+         * Comeca timer
+         */
         DisparaTimer();
-        
     }
 
     private void UpdateTentativas()
     {
+        /*
+         * Aumenta o numero de tentativas e atualiza o texto na tela
+         */
         numTentativas++;
         GameObject.Find("numTentativas").GetComponent<Text>().text = "Tentativas = " + numTentativas;
     }
 
     private void DisparaTimer()
     {
+        /*
+         * Aciona o timer
+         */
         timerPausado = false;
         timerAcionado = true;
     }
@@ -257,6 +294,9 @@ public class ManageCartas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
+         * Depois de um segundo do timer, Match é chamado para verificar as cartas selecionadas
+         */
         if(timerAcionado)
         {
             timer += Time.deltaTime;
@@ -273,18 +313,18 @@ public class ManageCartas : MonoBehaviour
 
     private void Match()
     {
-        for (int i = 0; i < cartasSelecionadasArray.Length; i++)
-        {
-
-        }
+        /*
+         * Verifica as condições de vitoria, reseta as cartas selecionadas, toca os sons de acerto e erro
+         */
         if (TagsIguais())
         {
-            for (int i = 0; i < cartasSelecionadasArray.Length; i++)
+            for (int i = 0; i < cartasSelecionadasArray.Length; i++)//Destroy selecionadas
             {
                 if (cartasSelecionadasArray[i]) Destroy(cartasSelecionadasArray[i]);
             }
+            somOK.Play();
             numAcertos++;
-            if (numAcertos == maxRankCartas)
+            if (numAcertos == maxRankCartas)//Condicao de vitoria
             {
                 PlayerPrefs.SetInt("Jogadas", numTentativas);
                 if(numTentativas < recorde || recorde == 0)
@@ -296,20 +336,22 @@ public class ManageCartas : MonoBehaviour
         }
         else
         {
-            for (int i = 0; i < cartasSelecionadasArray.Length; i++)
+            for (int i = 0; i < cartasSelecionadasArray.Length; i++)//Esconde selecionadas
             {
                 if (cartasSelecionadasArray[i]) cartasSelecionadasArray[i].GetComponent<Tile>().EscondeCarta();
             }
+            somErro.Play();
             UpdateTentativas();
         }
-        cartasSelecionadas = 0;
-        for (int i = 0; i < cartasSelecionadasArray.Length; i++)
+
+        cartasSelecionadas = 0;//Reseta bool
+        for (int i = 0; i < cartasSelecionadasArray.Length; i++)//Reset selecionadas
         {
             cartasSelecionadasArray[i] = null;
         }
         timer = 0;
 
-        bool TagsIguais()
+        bool TagsIguais()//Compara todas as tags das cartas selecionadas
         {
             for (int i = 0; i < cartasSelecionadasArray.Length; i++)
             {
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,62 +6,66 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     private bool tileRevelada = false; //indicador da carta virada ou não
-    public Sprite originalCarta;
-    public Sprite backCarta;
-    private SpriteRenderer spriteRenderer;
-    public int cartaID;
-    public int linhaID;
-    internal string naipe;
-    private static ManageCartas manager;
+    public Sprite originalCarta; //Face da carta
+    public Sprite backCarta; //Parte de tras da carta
+    private SpriteRenderer spriteRenderer; //Referencia para o sprite renderer para mudar os sprites
+    public int cartaID; //Mesma coisa que rank
+    public int linhaID; //Mesma coisa que tag
+    internal string naipe; //Guarda o naipe em string
+    private static ManageCartas manager; //referencia ao game manager
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
+         * condicao inial das cartas e set de referencias
+         */
         spriteRenderer = GetComponent<SpriteRenderer>();
         manager = GetComponentInParent<ManageCartas>();
         EscondeCarta();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void OnMouseDown()
     {
+        /*
+         * Ao clicar na carta o metodo de carta selecionada no manager e chamado
+         */
         print("Voce pressionou uma tile");
-        /*if (tileRevelada)
-        {
-            EscondeCarta();
-        }
-        else
-        {
-            RevelaCarta();
-        }*/
 
         manager.CartaSelecionada(gameObject);
     }
 
     public void EscondeCarta()
     {
+        /*
+         * Update do sprite para a carta virada para baixo
+         */
         spriteRenderer.sprite = backCarta;
         tileRevelada = false;
     }
 
     public void RevelaCarta()
     {
+        /*
+         * Update do sprite para a carta virada para cima
+         */
         spriteRenderer.sprite = originalCarta;
         tileRevelada = true;
     }
 
     public void setOriginalSprite(Sprite sprite)
     {
+        /*
+         * Update para um novo sprite da face
+         */
         originalCarta = sprite;
     }
 
     internal void setBackSprite(Sprite sprite)
     {
+        /*
+         * Update para um novo sprite da parte de tras
+         */
         backCarta = sprite;
     }
 }
